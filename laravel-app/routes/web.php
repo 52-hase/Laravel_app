548d8b8bd076e7ecebafcde312f34a53971e391f
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2025-03-10 03:38:14 +0900

    遷移先の変更

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login'); //最初の遷移ログイン
 });
 
 Route::get('/dashboard', function () {
